75dbf7c8d842f9243ea1423962c363abbedaaaeb
Sam Ievins <sam.ievins@adarga.ai>
2023-09-23 20:41:29 +0100

    various updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Rust
 local rt = require("rust-tools")
 
 rt.setup()
+
+-- Snippets
+require("luasnip").filetype_extend("typescript", { "javascript" })
+require("luasnip").filetype_extend("typescriptreact", { "javascript" })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,22 @@
 -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 -- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Move to window using the <ctrl> arrow keys
+vim.keymap.set("n", "<C-left>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-right>", "<C-w>l", { desc = "Go to right window", remap = true })
+vim.keymap.set("t", "<C-left>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+vim.keymap.set("t", "<C-down>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+vim.keymap.set("t", "<C-up>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+vim.keymap.set("t", "<C-right>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+
+-- Resize window using <ctrl> homing keys
+vim.keymap.set("n", "<C-pageup>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-pagedown>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-home>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-end>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
 -- Undo history
 vim.keymap.set("n", "<S-u>", vim.cmd.UndotreeToggle, { desc = "Undotree" })
 
@@ -13,10 +29,16 @@ vim.keymap.set("n", "<S-u>", vim.cmd.UndotreeToggle, { desc = "Undotree" })
 vim.keymap.set("x", "<S-p>", [["_dP]], { desc = "Paste (preserve copy)" })
 
 -- Markdown preview
-
 vim.keymap.set("n", "<M-m>", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Markdown preview" })
 
+-- Copy filename / path
+vim.keymap.set("n", "<leader>fpf", '<cmd>let @+ = expand("%:t")<cr>', { desc = "Copy filename" })
+vim.keymap.set("n", "<leader>fpp", '<cmd>let @+ = expand("%")<cr>', { desc = "Copy path" })
+
 -- Buffer
+vim.keymap.set("n", "<S-left>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<S-right>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+
 vim.keymap.set("n", "[b", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
 vim.keymap.set("n", "]b", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
 
