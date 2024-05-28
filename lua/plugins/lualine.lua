d539b78bcc616f2802e5882e37dfdf623f879785
Sam Ievins <sievins8012@gmail.com>
2024-05-28 14:12:16 +0100

    better lualine colors for when transparent

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,34 @@
+local custom_nightfly = require("lualine.themes.nightfly")
+
+-- Better nightfly colors for transparent background
+custom_nightfly.normal.a.bg = "#2c3043"
+custom_nightfly.normal.a.fg = "#82aaff"
+custom_nightfly.normal.b.bg = "#2c3043"
+custom_nightfly.normal.b.fg = "#82aaff"
+custom_nightfly.insert.a.bg = "#2c3043"
+custom_nightfly.insert.a.fg = "#21c7a8"
+custom_nightfly.insert.b.bg = "#2c3043"
+custom_nightfly.insert.b.fg = "#21c7a8"
+custom_nightfly.visual.a.bg = "#2c3043"
+custom_nightfly.visual.a.fg = "#ae81ff"
+custom_nightfly.visual.b.bg = "#2c3043"
+custom_nightfly.visual.b.fg = "#ae81ff"
+custom_nightfly.replace.a.bg = "#2c3043"
+custom_nightfly.replace.a.fg = "#ff5874"
+custom_nightfly.replace.b.bg = "#2c3043"
+custom_nightfly.replace.b.fg = "#ff5874"
+custom_nightfly.command = {
+  a = { bg = "#2c3043", fg = "#ecc48d" },
+  b = { bg = "#2c3043", fg = "#ecc48d" },
+}
+
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        theme = "solarized_dark",
+        -- theme = "nightfly",
+        theme = custom_nightfly,
 
         -- For transparent background
         section_separators = "",
