f22e64457c07a5fc4d6061429613fce35279f9a9
Sam Ievins <sievins8012@gmail.com>
2024-05-30 20:32:26 +0100

    rearrange windows

---
+++ lua/plugins/rearrange-windows.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "sindrets/winshift.nvim",
+    keys = {
+      { "<leader>we", "<cmd>WinShift<CR>", desc = "Edit Windows" },
+      { "<leader>ws", "<cmd>WinShift swap<CR>", desc = "Swap Windows" },
+    },
+  },
+}
